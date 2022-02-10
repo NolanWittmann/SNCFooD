118f2134e04bf41b764f9a191b725c26bbb00d19
Nolan Wittmann <wittmann.nolan@gmail.com>
2022-02-10 17:22:15 +0100

    footer non responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import Footer from "./components/Footer";
+
 
 
 function App() {
   return (
     <div className="App">
-     
+     <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import "./style/Footer.css"
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="bg-foot">
+            <p className='ancrage'>🠝</p>
+            <div className='leftPart'>
+                <h2 className='leftPart-h2'>sncfood</h2>
+                <ul>
+                    <li className='leftPart-li'><img src="../assets/fb.png" alt="" height="50px" /></li>
+                    <li className='leftPart-li'><img src="../assets/insta.png" alt="" height="50px" /></li>
+                    <li className='leftPart-li'><img src="../assets/snap.png" alt="" height="50px" /></li>
+                    <li className='leftPart-li'><img src="../assets/twiter.png" alt="" height="50px" /></li>
+                </ul>
+
+            </div>
+
+            <div className='centerPart'>
+
+                <h2 className='centerPart-h2'>NOS RECETTES</h2>
+                <ul>
+                    <li className='centerPart-li'>Blanquette de veaux</li>
+                    <li className='centerPart-li'>Quiche Lorraine</li>
+                    <li className='centerPart-li'>Gateau au Yaourt</li>
+                    <li className='centerPart-li'>Gratin Dauphinois</li>
+                    <li className='centerPart-li'>Gateau au Chocolat</li>
+                    <li className='centerPart-li'>Tiramisu</li>
+                    <li className='centerPart-li'>Pate a Crepe</li>
+                    <li className='centerPart-li'>Boeuf Bourguignon</li>
+                    <li className='centerPart-li'>Tarte aux Pommes</li>
+                    <li className='centerPart-li'>Mousse au Chocolat</li>
+                    <li className='centerPart-li'>Pain Perdu</li>
+                    <li className='centerPart-li'>Poulet Basquaise</li>
+                    <li className='centerPart-li'>Tomates Farcies</li>
+                    <li className='centerPart-li'>Pot au Feu</li>
+                </ul>
+            </div>
+
+            <div className="rightPart">
+                <h2 className='rightPart-h2'>CATEGORIES</h2>
+                <ul>
+                    <li className='rightPart-li'>Recettes sans Gluten</li>
+                    <li className='rightPart-li'>Recettes Vegan</li>
+                    <li className='rightPart-li'>Recettes Végétariennes</li>
+                    <li className='rightPart-li'>Recettes sans Porc</li>
+                </ul>
+            </div>
+
         </div>
     );
 };
