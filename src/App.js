37c9b76d8245ce9506e2eddcab05b6b4604ae132
Manon VAVASSEUR <Manon.vavasseur@live.fr>
2022-02-11 09:41:42 +0100

    commencement navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import Navigation from "./components/Navigation";
+
 
 
 function App() {
   return (
     <div className="App">
-     
+     <Navigation />
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState} from 'react';
+import "./Navigation.css";
+
+
 
 const Navigation = () => {
+
+    const [menuBurger, setMenuBurger] = useState(false)
+    
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <div className='nav-btn'>
+                <button className='btn__color' onClick={() =>  setMenuBurger(!menuBurger) }  > 🍔 </button>
+            </div>
+            {menuBurger &&
+            <ul className='liste'>
+                <li className='liste__li'><a href="liste__ancrage">Comment Faire ?</a></li>
+                <li className='liste__li'><a href="liste__ancrage">Questionnaire </a> </li>
+    
+            </ul> }
+           <ul className='liste__big__screen'>
+                <li className='liste__li__big'><a href="# liste__ancrage">Comment Faire ?</a></li>
+                <li className='liste__li__big'><a href="#liste__ancrage">Questionnaire  </a></li>
+            </ul> 
+        </nav>
     );
 };
 
