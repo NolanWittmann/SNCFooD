9e7a7785bff9e6bbbedb42f6474fdf9f8c34e01e
Manon VAVASSEUR <Manon.vavasseur@live.fr>
2022-02-16 10:38:10 +0100

    barre quasi fini

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
      <Navigation />
     </div>
+    
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,7 @@ import "./Navigation.css";
 
 
 
+
 const Navigation = () => {
 
     const [menuBurger, setMenuBurger] = useState(false)
@@ -12,15 +13,16 @@ const Navigation = () => {
             <div className='nav-btn'>
                 <button className='btn__color' onClick={() =>  setMenuBurger(!menuBurger) }  > 🍔 </button>
             </div>
+        
             {menuBurger &&
             <ul className='liste'>
-                <li className='liste__li'><a href="liste__ancrage">Comment Faire ?</a></li>
-                <li className='liste__li'><a href="liste__ancrage">Questionnaire </a> </li>
-    
+                <li className='liste__li'><a href="">Comment Faire ?</a></li>
+                <li className='liste__li'><a href="">Questionnaire </a> </li>
             </ul> }
+            
            <ul className='liste__big__screen'>
-                <li className='liste__li__big'><a href="# liste__ancrage">Comment Faire ?</a></li>
-                <li className='liste__li__big'><a href="#liste__ancrage">Questionnaire  </a></li>
+                <li className='liste__li__big'><a href="">Comment Faire ?</a></li>
+                <li className='liste__li__big'><a href="">Questionnaire  </a></li>
             </ul> 
         </nav>
     );
