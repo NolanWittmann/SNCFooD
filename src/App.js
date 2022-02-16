56937ef1ddbd5efa57be260bb9d7f66cb73d528d
Manon VAVASSEUR <Manon.vavasseur@live.fr>
2022-02-16 11:12:59 +0100

    test recup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Footer from "./components/Footer";
-import "./components/style/App.css
+import "./components/style/App.css"
 import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <div id="ok"></div>
       <Footer />
     </div>
     
