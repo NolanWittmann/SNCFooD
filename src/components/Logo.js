e6ed9fa535c4a3733fc174b1f43cd1cd38758840
Nicolas Bouchet <93680038+ChuNicolasBouchet@users.noreply.github.com>
2022-02-17 15:17:32 +0100

    asset vers svg

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import "../style/Logo.css";
-import { ReactComponent as Logofood } from '../assets/sncfood_v2.svg';
+import "/style/Logo.css";
+import { ReactComponent as Logofood } from '/svg/sncfood_v2.svg';
 
 const Logo = () => {
     return (
         <div className="logo_header">
-            
             <div>
                 <Logofood height="14rem"/>
             </div>
-        
         </div>
     );
 };
