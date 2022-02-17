5835613b69d78115bb1a6090d6a6c8e1adc5ee4c
Nicolas Bouchet <93680038+ChuNicolasBouchet@users.noreply.github.com>
2022-02-17 11:46:34 +0100

    ajout du logo en haut

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { ReactComponent as Logofood } from '.assets/sncfood_v2.svg';
 
 const Logo = () => {
     return (
         <div>
-            
+            <Logofood height="5rem" />
         </div>
     );
 };
