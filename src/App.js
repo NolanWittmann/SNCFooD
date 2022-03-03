2bd5d8a4889afa46b877e2352d398cb70914a426
Nicolas Bouchet <93680038+ChuNicolasBouchet@users.noreply.github.com>
2022-03-03 09:39:35 +0100

    response & responsecard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import React from "react";
+import Questionnaire from "./components/Questionnaire.js";
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
+import Response from "./components/Response.js";
 
 function App() {
   return (
     <div className="App">
-     
+      <Navigation />
+      <Questionnaire />
+      <Response />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Response.js
@@ -0,0 +1,113 @@
+import "../style/Reponse.css"
+import React, {useEffect, useState} from 'react';
+import Axios from 'axios';
+import Recipecard from "./Recipecard"
+
+function Response() {
+    const [recipes, setRecipes] = useState([])
+    const [query, setQuery] = useState("");
+    const [healthLabel, setHealthLabel] = useState("vegetarian");
+
+    
+
+    const url = `https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=${query}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&health=${healthLabel}`
+
+    const getRecipeInfo = async () => {
+        const result = await Axios.get(url);
+        setRecipes(result.data.hits);
+        console.log(result.data.hits);
+    };
+
+    useEffect(() => {
+      getRecipeInfo();
+    }, [query]);
+  
+    const onSubmit = (e) => {
+        e.preventDefault();
+        getRecipeInfo();
+    };
+
+    return (
+        <div className ="response">
+          <h1 onClick={getRecipeInfo}></h1>
+          <form className="response__searchForm" onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Tapez un ingrédient"
+              autoComplete="Off"
+              className="recipe__input"
+              value={query}
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
+            />
+            <select className="recipe__healthLabel">
+            <option
+                value=""
+                onClick={() => {
+                  setHealthLabel("");
+                }}
+                >
+                Sans particularité
+              </option>
+              <option
+                value="vegetarian"
+                onClick={() => {
+                  setHealthLabel("vegetarian");
+                }}
+                >
+                Végétarien
+              </option>
+              <option
+                value="gluten-free"
+                onClick={() => {
+                  setHealthLabel("gluten-free");
+                }}
+                >
+                Sans gluten
+              </option>
+              <option
+                value="pork-free"
+                onClick={() => {
+                  setHealthLabel("pork-free");
+                }}
+                >
+                Sans porc
+              </option>
+              <option
+                value="peanut-free"
+                onClick={() => {
+                  setHealthLabel("peanut-free");
+                }}
+                >
+                Sans arachides
+              </option>
+              <option
+                value="crustacean-free"
+                onClick={() => {
+                  setHealthLabel("crustacean-free");
+                }}
+                >
+                Sans crustacés
+              </option>
+            </select>
+        <input type="submit" value="Obtenez une recette" className="recipe__submit" />
+      </form>
+
+      <div className="response__recipes">
+        {recipes.map((recipe) => {
+          return <Recipecard 
+                recipe={recipe}
+                key={recipe.recipe.label}
+                title={recipe.recipe.label}
+                image={recipe.recipe.image}
+                ingredients={recipe.recipe.ingredients}
+           />;
+        })}
+      </div>
+
+    </div>
+  );
+};
+
+export default Response;
